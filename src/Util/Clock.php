45e4522d9e92149783648c0fbd12f6d9b061084e
Aaron <aaronidasbr@gmail.com>
2020-04-26 12:35:32 +0200

    Remove author comments

--- src/Util/Clock.php
+++ src/Util/Clock.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * This disaster was designed by
- *
- * @author Juan G. Rodríguez Carrión <juan.rodriguez@pccomponentes.com>
- */
 declare(strict_types=1);
 
 namespace PcComponentes\Ddd\Util;
