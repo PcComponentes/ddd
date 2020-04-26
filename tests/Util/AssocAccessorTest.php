5cf68a4586e1eb38b6dfda5f80aceb4eea53423a
Aaron <aaronidasbr@gmail.com>
2020-04-26 12:57:48 +0200

    Remove author comments in test directory

--- tests/Util/AssocAccessorTest.php
+++ tests/Util/AssocAccessorTest.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * This disaster was designed by
- * @author Juan G. Rodríguez Carrión <juan.rodriguez@pccomponentes.com>
- */
+
 declare(strict_types=1);
 namespace PcComponentes\Ddd\Tests\Util;
 
