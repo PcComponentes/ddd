6aff5430b4965810ba977fcef2bb9a3bb7cbb905
Julian Torices <julian.torices@pccomponentes.com>
2023-07-06 13:41:50 +0200

    refactor: rename wrong class name

--- src/Domain/Model/ValueObject/DateTimeRangeValeObject.php
+++ src/Domain/Model/ValueObject/DateTimeRangeValeObject.php
@@ -3,69 +3,7 @@ declare(strict_types=1);
 
 namespace PcComponentes\Ddd\Domain\Model\ValueObject;
 
-class DateTimeRangeValeObject implements ValueObject
+/** @deprecated (typo in name) use DateTimeRangeValueObject instead */
+class DateTimeRangeValeObject extends DateTimeRangeValueObject
 {
-    private DateTimeValueObject $start;
-    private DateTimeValueObject $end;
-
-    final private function __construct(DateTimeValueObject $start, DateTimeValueObject $end)
-    {
-        $this->start = $start;
-        $this->end = $end;
-    }
-
-    public static function from(DateTimeValueObject $start, DateTimeValueObject $end): static
-    {
-        if ($start >= $end) {
-            throw new \InvalidArgumentException('Start date can not be greater than the end date.');
-        }
-
-        return new static($start, $end);
-    }
-
-    public function start(): DateTimeValueObject
-    {
-        return $this->start;
-    }
-
-    public function end(): DateTimeValueObject
-    {
-        return $this->end;
-    }
-
-    public function isInDate(): bool
-    {
-        $now = DateTimeValueObject::now();
-
-        return $now >= $this->start && $now <= $this->end;
-    }
-
-    public function isExpired(): bool
-    {
-        return DateTimeValueObject::now() > $this->end;
-    }
-
-    public function isNotStarted(): bool
-    {
-        return DateTimeValueObject::now() < $this->start;
-    }
-
-    public function equalTo(DateTimeRangeValeObject $another): bool
-    {
-        return $this->start->getTimestamp() === $another->start->getTimestamp()
-            && $this->end->getTimestamp() === $another->end->getTimestamp();
-    }
-
-    public function jsonSerialize(): array
-    {
-        return $this->value();
-    }
-
-    public function value(): mixed
-    {
-        return [
-            'start' => $this->start,
-            'end' => $this->end,
-        ];
-    }
 }
