4fae13a7f0d687d1c25b1d9b4bf3419898deac6a
Julian Torices <julian.torices@pccomponentes.com>
2025-01-23 11:47:08 +0100

    feat: delete deprecated DateTimeRangeValeObject

--- src/Domain/Model/ValueObject/DateTimeRangeValeObject.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace PcComponentes\Ddd\Domain\Model\ValueObject;
-
-/** @deprecated (typo in name) use DateTimeRangeValueObject instead */
-class DateTimeRangeValeObject extends DateTimeRangeValueObject
-{
-}
