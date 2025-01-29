21cd7d6bbe53d9dc3221bd7957fb72159b163b33
Julian Torices <julian.torices@pccomponentes.com>
2025-01-29 11:44:17 +0100

    Revert "Merge pull request #43 from PcComponentes/feature/delete-deprecated-DateTimeRangeValeObject"
    This reverts commit 3ec2fae3aea2c9461bf351c15747da695db20334, reversing
    changes made to 3e2fcce66557c775f3bcbf8d8a9f9325dbffe8fd.

---
+++ src/Domain/Model/ValueObject/DateTimeRangeValeObject.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace PcComponentes\Ddd\Domain\Model\ValueObject;
+
+/** @deprecated (typo in name) use DateTimeRangeValueObject instead */
+class DateTimeRangeValeObject extends DateTimeRangeValueObject
+{
+}
